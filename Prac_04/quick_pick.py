732f5b16780d40e28f1ff5987b162e16616508a3
Tony <tony.li@my.jcu.edu.au>
2018-08-23 15:47:19 +1000

    prac4 finished

--- Prac_04/quick_pick.py
+++ Prac_04/quick_pick.py
@@ -1,3 +1,5 @@
+import random
+
 numbers_per_line = 6
 min = 1
 max = 45
@@ -7,4 +9,14 @@ while number_of_picks < 0:
     print("Pick a Positive number")
     number_of_picks = int(input('How many quick picks? '))
 
-for
+for i in range(number_of_picks):
+    picks = []
+    for j in range(numbers_per_line):
+        number = random.randint(min,max)
+        if number in picks:
+            number = random.randint(min, max)
+        picks.append(number)
+        picks.sort()
+    print(picks)
+
+
